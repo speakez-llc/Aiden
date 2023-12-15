e29205cba6392064445a5464d56f7b81502bf779
Houston Haynes (Rower) <h3techsme@gmail.com>
2023-12-15 16:51:26 -0500

    Getting rid of bad advice

--- src/AidenDesktop/ViewModels/ChatViewModel.fs
+++ src/AidenDesktop/ViewModels/ChatViewModel.fs
@@ -4,16 +4,18 @@ open Elmish
 open ReactiveElmish
 open ReactiveElmish.Avalonia
 open DynamicData
-open System.Collections.Generic
 open System.Collections.ObjectModel
+open System
 
 module Chat =
     type Message = { User: string; Text: string; Alignment: string; Color: string; BorderColor: string }
 
-    type Model = { Messages: SourceList<Message> }
+    type Model = { Messages: SourceList<Message>; NewMessage: string }
 
     type Msg =
-        | SendMessage of Message
+        | SendMessage of string
+        | UpdateNewMessage of string
+
 
     let init() =
         let initialMessages =
@@ -28,37 +30,36 @@ module Chat =
                   Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
                 { User = "Aiden"; Text = "Thank you. I will continue to scan for news and monitor and notify on any DDoS activity."
                   Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Gray"  }
-                { User = "Aiden"; Text = "Anomaly detected in in-bound data. It mirrors a previous probe attack that has preceded a DDoS cycle by 20 minutes."
-                  Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Orange"  }
-                { User = "Me"; Text = "Thanks I see it. Clear the alarm. Is there any news on the wire that this is hitting more than us?"
-                  Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
-                { User = "Aiden"; Text = "No headlines have been found in a recent sweep of known sources."
-                  Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Gray"  }
-                { User = "Me"; Text = "OK. I have issued a request to re-route traffic. You can revert the status to green."
-                  Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
-                { User = "Aiden"; Text = "Thank you. I will continue to scan for news and monitor and notify on any DDoS activity."
-                  Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Gray"  }
             ]
-        { Messages = SourceList.createFrom initialMessages }
+        { Messages = SourceList.createFrom initialMessages; NewMessage = ""}
 
     let update (msg: Msg) (model: Model) =
         match msg with
-        | SendMessage message -> model.Messages |> SourceList.add message; model
+        | SendMessage text ->
+            model.Messages |> SourceList.add { User = "Me"; Text = text; Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue" }
+            { model with NewMessage = "" }
+        | UpdateNewMessage text -> { model with NewMessage = text }
 
+        
 open Chat
-open System.Collections.Generic
 
 type ChatViewModel() as this =
     inherit ReactiveElmishViewModel()
 
-    let local = 
+    let local =
         Program.mkAvaloniaSimple init update
         |> Program.withErrorHandler (fun (_, ex) -> printfn "Error: %s" ex.Message)
         |> Program.mkStore
     let messages = ObservableCollection<Message>(this.BindSourceList(local.Model.Messages))
 
-    member x.LastMessage 
+    member x.LastMessage
         with get() = Seq.last x.MessagesView
     member this.MessagesView: ObservableCollection<Message> = messages
+
+    member this.NewMessage = this.Bind (local, _.NewMessage)
+    
+    member this.SendMessage() =
+        local.Dispatch (SendMessage this.NewMessage)
+        printfn "Sending message: %A" this.NewMessage
 
     static member DesignVM = new ChatViewModel()
