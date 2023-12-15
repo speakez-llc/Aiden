5454d4afe5041925b0e52f6e29b158f16e9c3eb0
Houston Haynes (Rower) <h3techsme@gmail.com>
2023-12-15 18:08:18 -0500

    simplify SourceList.add for Messages

--- src/AidenDesktop/ViewModels/ChatViewModel.fs
+++ src/AidenDesktop/ViewModels/ChatViewModel.fs
@@ -22,20 +22,16 @@ module Chat =
                   Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Orange"  }
                 { User = "Me"; Text = "Thanks I see it. Clear the alarm. Is there any news on the wire that this is hitting more than us?"
                   Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
-                { User = "Aiden"; Text = "No headlines have been found in a recent sweep of known sources."
-                  Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Gray"  }
-                { User = "Me"; Text = "OK. I have issued a request to re-route traffic. You can revert the status to green."
-                  Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
-                { User = "Aiden"; Text = "Thank you. I will continue to scan for news and monitor and notify on any DDoS activity."
-                  Alignment = "Left"; Color = "WhiteSmoke"; BorderColor = "Gray"  }
             ]
         { Messages = SourceList.createFrom initialMessages}
 
     let update (msg: Msg) (model: Model) =
         match msg with
         | SendMessage text ->
+            let msg = { User = "Me"; Text = text; Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
+            printfn "Message: %A" msg
             {
-                Messages = model.Messages |> SourceList.add { User = "Me"; Text = text; Alignment = "Right"; Color = "White"; BorderColor = "MidnightBlue"  }
+                Messages = model.Messages |> SourceList.add msg
             }
 
 open Chat
@@ -47,13 +43,11 @@ type ChatViewModel() as this =
         Program.mkAvaloniaSimple init update
         |> Program.withErrorHandler (fun (_, ex) -> printfn "Error: %s" ex.Message)
         |> Program.mkStore
+    
     let messages = ObservableCollection<Message>(this.BindSourceList(local.Model.Messages))
-
-    member x.LastMessage
-        with get() = Seq.last x.MessagesView
     member this.MessagesView: ObservableCollection<Message> = messages
     
     member this.SendMessage(message: string) =
         local.Dispatch (SendMessage message)
-
+        
     static member DesignVM = new ChatViewModel()
