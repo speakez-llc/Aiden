4619ddfae17690eaf2bc6e1aa380b6791fdcaa1f
Houston Haynes (Rower) <h3techsme@gmail.com>
2023-12-17 16:36:40 -0500

    Update to use FluentAvaloniaUI

--- src/AidenDesktop/Views/MainView.axaml.fs
+++ src/AidenDesktop/Views/MainView.axaml.fs
@@ -3,9 +3,10 @@ namespace AidenDesktop.Views
 open Avalonia
 open Avalonia.Controls
 open Avalonia.Markup.Xaml
+open FluentAvalonia.UI.Windowing
 
 type MainView () as this = 
-    inherit Window ()
+    inherit AppWindow ()
 
     do this.InitializeComponent()
 
