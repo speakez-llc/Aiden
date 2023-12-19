bd274d21590b6f0a5adde4207ad4ba5b3d02434a
Houston Haynes (Rower) <h3techsme@gmail.com>
2023-12-19 11:01:29 -0500

    chat scroll fixed

--- src/AidenDesktop/Views/ChatView.axaml.fs
+++ src/AidenDesktop/Views/ChatView.axaml.fs
@@ -1,38 +1,43 @@
 ﻿namespace AidenDesktop.Views
 
+open Avalonia
 open Avalonia.Controls
 open Avalonia.Markup.Xaml
 open Avalonia.Threading
 open AidenDesktop.ViewModels
-open System
+open System.Threading.Tasks
 
 type ChatView() as this =
     inherit UserControl ()
 
     let mutable viewModel: ChatViewModel option = None
 
-    do this.InitializeComponent()
-
-    override this.OnDataContextChanged args =
-        base.OnDataContextChanged args
-        viewModel <- this.DataContext :?> ChatViewModel |> Some
-        match viewModel with
-        | Some viewModel ->
-            viewModel.NewMessageEvent
-            |> Observable.subscribe (fun _ -> this.ScrollToBottom())
-            |> ignore
-        | None -> ()
+    do
+        this.InitializeComponent()
+        this.DataContextChanged.Add(fun args ->
+            viewModel <- this.DataContext :?> ChatViewModel |> Some
+            match viewModel with
+            | Some viewModel ->
+                viewModel.NewMessageEvent
+                |> Observable.subscribe (fun _ ->
+                    printfn "NewMessageEvent triggered"
+                    this.ScrollToBottom())
+                |> ignore
+            | None -> ())
 
     member private this.ScrollToBottom() =
+        printfn "ScrollToBottom called"
         let listBox = this.FindControl<ListBox>("ChatWindow")
-        if listBox.ItemCount > 0 then
-            let timer = new DispatcherTimer(DispatcherPriority.Loaded)
-            timer.Interval <- TimeSpan.FromMilliseconds(100.0)
-            timer.Tick.Subscribe(fun _ ->
-                listBox.ScrollIntoView(listBox.Items[listBox.ItemCount - 1])
-                timer.Stop())
-            timer.Start()
+        Dispatcher.UIThread.InvokeAsync((fun () ->
+            if listBox.ItemCount > 0 then
+                Task.Delay(100).ContinueWith(fun _ ->
+                    Dispatcher.UIThread.InvokeAsync((fun () ->
+                        printfn "Scrolling to bottom"
+                        let item = listBox.ContainerFromIndex(listBox.ItemCount - 1)
+                        (item :?> ListBoxItem).BringIntoView()
+                    ), DispatcherPriority.Loaded) |> ignore
+                ) |> ignore
+        ), DispatcherPriority.Loaded) |> ignore
 
     member private this.InitializeComponent() =
         AvaloniaXamlLoader.Load(this)
-
