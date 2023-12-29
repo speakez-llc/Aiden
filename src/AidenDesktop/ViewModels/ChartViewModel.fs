cd69abb91acc3d5893c991cc2d3b29b06ef6989a
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2023-12-28 22:10:02 -0500

    Event time series working

--- src/AidenDesktop/ViewModels/ChartViewModel.fs
+++ src/AidenDesktop/ViewModels/ChartViewModel.fs
@@ -1,9 +1,14 @@
 namespace AidenDesktop.ViewModels
 
 open System
+open System.Data
+open System.IO
 open System.Collections.Generic
 open System.Collections.ObjectModel
 open System.Reactive.Linq
+open System.Text.Json
+open Avalonia.Markup.Xaml.MarkupExtensions.CompiledBindings
+open LiveChartsCore.SkiaSharpView.Drawing.Geometries
 open ReactiveElmish
 open ReactiveElmish.Avalonia
 open Elmish
@@ -12,30 +17,156 @@ open LiveChartsCore
 open LiveChartsCore.Kernel.Sketches
 open LiveChartsCore.SkiaSharpView
 open LiveChartsCore.Defaults
+open Npgsql
 
 module Chart = 
 
     let rnd = Random()
+    
+    let connectionString =
+        let filePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json")
+        if File.Exists(filePath) then
+            let json = JsonDocument.Parse(File.ReadAllText(filePath))
+            let dbSection = json.RootElement.GetProperty("Database")
+            let connectionString = dbSection.GetProperty("ConnectionString").GetString()
+            //printfn $"Connection String: %s{connectionString}"
+            connectionString
+        else
+            printfn $"Error: File not found: %s{filePath}"
+            ""
+            
+    let fetchEventsAsync() =
+        async {
+            // Connect to the database and execute the query
+            use connection = new NpgsqlConnection(connectionString)
+            do! connection.OpenAsync() |> Async.AwaitTask
+            // Construct the query string with the column name
+            let query =
+                $@"SELECT event_time, src_ip, src_port, dst_ip, dst_port, cc, vpn, proxy, tor, malware  
+                   FROM events_hourly
+                   WHERE event_time >= now() AT TIME ZONE 'UTC' - INTERVAL '1 minute'
+                   ORDER BY event_time DESC;"
 
-    let newSeries (count: int option)  =
-        let newCollection = ObservableCollection<DateTimePoint>()
-        // use seriesCount to either 1) set a default 15 at init or 2) use the count passed in from Reset 
-        let mutable seriesCount = 0
-        match count with
-        | None ->
-            seriesCount <- 15
-        | _ -> 
-            seriesCount <- count.Value - 1
-        for i = seriesCount downto 0 do
-            // backdate the time in seconds by the index to create a series of points in the past
-            let past = DateTimeOffset.Now.AddSeconds(-i).LocalDateTime
-            let randomNull = rnd.Next(0, 99)
-            // in 1% of cases produce a null value to show an "empty" spot in the series
-            match randomNull with
-                | i when i = 0 ->
-                    newCollection.Add(DateTimePoint(past, System.Nullable()))
-                | _ -> newCollection.Add(DateTimePoint(past, rnd.Next(0, 10)))
-        newCollection
+            use cmd = new NpgsqlCommand(query, connection)
+
+            do! cmd.PrepareAsync() |> Async.AwaitTask
+
+            use! reader = cmd.ExecuteReaderAsync() |> Async.AwaitTask
+            let results = 
+                [ while reader.Read() do
+                    yield (
+                        reader.GetDateTime(reader.GetOrdinal("event_time")),
+                        reader.GetString(reader.GetOrdinal("src_ip")),
+                        reader.GetInt32(reader.GetOrdinal("src_port")),
+                        reader.GetString(reader.GetOrdinal("dst_ip")),
+                        reader.GetInt32(reader.GetOrdinal("dst_port")),
+                        reader.GetString(reader.GetOrdinal("cc")),
+                        reader.GetString(reader.GetOrdinal("vpn")),
+                        reader.GetString(reader.GetOrdinal("proxy")),
+                        reader.GetString(reader.GetOrdinal("tor")),
+                        reader.GetString(reader.GetOrdinal("malware"))
+                    ) ]
+            return results
+        }
+        
+    let fetchEventsPerSecondAsync() =
+        async {
+            // Connect to the database and execute the query
+            use connection = new NpgsqlConnection(connectionString)
+            do! connection.OpenAsync() |> Async.AwaitTask
+            // Construct the query string with the column name
+            let query =
+                $@"WITH time_series AS (
+                    SELECT generate_series(
+                        date_trunc('second', now() AT TIME ZONE 'UTC') - INTERVAL '1 minute',
+                        date_trunc('second', now() AT TIME ZONE 'UTC'),
+                        '1 second'::interval
+                    ) AS event_time
+                )
+                SELECT
+                    time_series.event_time,
+                    COALESCE(events.count, 0) AS count
+                FROM
+                    time_series
+                LEFT JOIN (
+                    SELECT
+                        date_trunc('second', event_time) AS event_time,
+                        COUNT(*) AS count
+                    FROM
+                        events_hourly
+                    WHERE
+                        event_time >= now() AT TIME ZONE 'UTC' - INTERVAL '1 minute'
+                    GROUP BY
+                        date_trunc('second', event_time)
+                ) AS events
+                ON time_series.event_time = events.event_time
+                    ORDER BY
+                        time_series.event_time ASC;"
+
+            use cmd = new NpgsqlCommand(query, connection)
+
+            do! cmd.PrepareAsync() |> Async.AwaitTask
+
+            use! reader = cmd.ExecuteReaderAsync() |> Async.AwaitTask
+            let results =
+                [ while reader.Read() do
+                    yield (
+                        reader.GetDateTime(reader.GetOrdinal("event_time")),
+                        reader.GetInt32(reader.GetOrdinal("count"))
+                    ) ]
+            return results
+        }
+        
+    let fetchMALEventsPerSecondAsync() =
+        async {
+            // Connect to the database and execute the query
+            use connection = new NpgsqlConnection(connectionString)
+            do! connection.OpenAsync() |> Async.AwaitTask
+            // Construct the query string with the column name
+            let query =
+                $@"WITH time_series AS (
+                    SELECT generate_series(
+                        date_trunc('second', now() AT TIME ZONE 'UTC') - INTERVAL '1 minute',
+                        date_trunc('second', now() AT TIME ZONE 'UTC'),
+                        '1 second'::interval
+                    ) AS event_time
+                )
+                SELECT
+                    time_series.event_time,
+                    COALESCE(events.count, 0) AS count
+                FROM
+                    time_series
+                LEFT JOIN (
+                    SELECT
+                        date_trunc('second', event_time) AS event_time,
+                        COUNT(*) AS count
+                    FROM
+                        events_hourly
+                    WHERE
+                        event_time >= now() AT TIME ZONE 'UTC' - INTERVAL '1 minute'
+                        AND malware in ('TRUE')
+                    GROUP BY
+                        date_trunc('second', event_time)
+                ) AS events
+                ON time_series.event_time = events.event_time
+                    ORDER BY
+                        time_series.event_time ASC;"
+
+            use cmd = new NpgsqlCommand(query, connection)
+
+            do! cmd.PrepareAsync() |> Async.AwaitTask
+
+            use! reader = cmd.ExecuteReaderAsync() |> Async.AwaitTask
+            let results =
+                [ while reader.Read() do
+                    yield (
+                        reader.GetDateTime(reader.GetOrdinal("event_time")),
+                        reader.GetInt32(reader.GetOrdinal("count"))
+                    ) ]
+            return results
+        }
+
+    
     
     // create time labeling for the X axis in the Chart visual
     let XAxes : IEnumerable<ICartesianAxis> =
@@ -50,75 +181,78 @@ module Chart =
     type Model = 
         {
             Series: ObservableCollection<ISeries>
-            Actions: SourceList<Action>
-            IsAutoUpdateChecked: bool
-        }
-    
-    and Action = 
-        {
-            Description: string
-            Timestamp: DateTime
         }
 
-    type Msg = 
-        | AddItem
-        | AddNull
-        | RemoveItem
-        | UpdateItem
-        | ReplaceItem
-        | Reset
-        | SetIsAutoUpdateChecked of bool
-        | Terminate
+
+    type Msg =
+       | RemoveOldSeries
+       | UpdateSeries
+       | Terminate
     
     let init() =
+        let eventsPerSecond = 
+            fetchEventsPerSecondAsync()
+            |> Async.RunSynchronously
+            |> List.map (fun (time, count) -> DateTimePoint(time, float count))
+            |> ObservableCollection<_>
+
+        let malEventsPerSecond = 
+            fetchMALEventsPerSecondAsync()
+            |> Async.RunSynchronously
+            |> List.map (fun (time, count) -> DateTimePoint(time, float count))
+            |> ObservableCollection<_>
+
         {
-            Series = ObservableCollection<ISeries> 
-                [ 
-                    LineSeries<DateTimePoint>(Values = newSeries(None), Name = "Luck By Second") :> ISeries 
+            Series = ObservableCollection<ISeries>
+                [
+                    LineSeries<DateTimePoint>(Values = eventsPerSecond, Name = "Events per Second", GeometryFill = null, GeometryStroke = null) :> ISeries
+                    ColumnSeries<DateTimePoint>(Values = malEventsPerSecond, Name = "MAL Events/Sec") :> ISeries
                 ]
-            Actions = SourceList.createFrom [ { Description = "Initialized Chart"; Timestamp = DateTime.Now }]
-            IsAutoUpdateChecked = true
         }
 
     let update (msg: Msg) (model: Model) =
-        let values = model.Series[0].Values :?> ObservableCollection<DateTimePoint>
         match msg with
-        | AddItem ->
-            values.Insert(values.Count, DateTimePoint(DateTime.Now, rnd.Next(0, 10)))
-            { model with 
-                Actions = model.Actions |> SourceList.add { Description = $"Added Item"; Timestamp = DateTime.Now } }
-        | AddNull ->
-            values.Insert(values.Count, DateTimePoint(DateTime.Now, System.Nullable()))
-            { model with 
-                Actions = model.Actions |> SourceList.add { Description = $"Added Null"; Timestamp = DateTime.Now } }
-        | RemoveItem ->
-            values.RemoveAt(0)
-            { model with 
-                Actions = model.Actions |> SourceList.add { Description = $"Removed Item"; Timestamp = DateTime.Now } }
-        | UpdateItem ->
-            let item = rnd.Next(0, values.Count - 1)
-            let fstValueTime = values[item].DateTime
-            values[item] <- DateTimePoint(fstValueTime, rnd.Next(0, 10))
-            { model with 
-                Actions = model.Actions |> SourceList.add { Description = $"Updated Item: {item + 1}"; Timestamp = DateTime.Now } }
-        | ReplaceItem ->
-            let lastValueTime = values[values.Count - 1].DateTime
-            values[values.Count - 1] <- DateTimePoint(lastValueTime, rnd.Next(0, 10))
-            { model with 
-                Actions = model.Actions |> SourceList.add { Description = $"Replaced Item: {values.Count}"; Timestamp = DateTime.Now } }
-        | Reset ->
-            // insert new Series - send the current series length to the newSeries function
-            model.Series[0].Values <- newSeries(Some values.Count)
-            { model with
-                // deactivate the AutoUpdate ToggleButton in the UI
-                IsAutoUpdateChecked = false 
-                Actions = model.Actions |> SourceList.removeAll |> SourceList.add { Description = "Reset Chart"; Timestamp = DateTime.Now }
-            }
-        | SetIsAutoUpdateChecked isChecked ->
-            { model with 
-                IsAutoUpdateChecked = isChecked
-                Actions = model.Actions |> SourceList.add { Description = $"Is AutoUpdate Checked: {isChecked}"; Timestamp = DateTime.Now }
-            }
+        | RemoveOldSeries ->
+            let cutoff = DateTime.UtcNow.AddSeconds(-60.0)
+            let values1 = model.Series.[0].Values :?> ObservableCollection<DateTimePoint>
+            let values2 = model.Series.[1].Values :?> ObservableCollection<DateTimePoint>
+
+            // Find the elements to remove
+            let oldValues1 = values1 |> Seq.filter (fun point -> point.DateTime < cutoff) |> Seq.toList
+            let oldValues2 = values2 |> Seq.filter (fun point -> point.DateTime < cutoff) |> Seq.toList
+
+            // Remove the old elements
+            oldValues1 |> List.iter (fun point -> ignore (values1.Remove point))
+            oldValues2 |> List.iter (fun point -> ignore (values2.Remove point))
+
+            model
+        | UpdateSeries ->
+            let latestEvents =
+                fetchEventsPerSecondAsync()
+                |> Async.RunSynchronously
+                |> List.map (fun (time, count) -> DateTimePoint(time, float count))
+
+            let malEventsPerSecond =
+                fetchMALEventsPerSecondAsync()
+                |> Async.RunSynchronously
+                |> List.map (fun (time, count) -> DateTimePoint(time, float count))
+
+            // Update existing data points and add new ones
+            let values1 = model.Series.[0].Values :?> ObservableCollection<DateTimePoint>
+            let values2 = model.Series.[1].Values :?> ObservableCollection<DateTimePoint>
+            latestEvents
+            |> List.iter (fun point ->
+                match Seq.tryFind (fun (p: DateTimePoint) -> p.DateTime = point.DateTime) values1 with
+                | Some existingPoint -> existingPoint.Value <- point.Value
+                | None -> values1.Insert(values1.Count, point))
+
+            malEventsPerSecond
+            |> List.iter (fun point ->
+                match Seq.tryFind (fun (p: DateTimePoint) -> p.DateTime = point.DateTime) values2 with
+                | Some existingPoint -> existingPoint.Value <- point.Value
+                | None -> values2.Insert(values2.Count, point))
+
+            model
         | Terminate ->
             model
 
@@ -126,20 +260,12 @@ module Chart =
         let autoUpdateSub (dispatch: Msg -> unit) = 
             Observable
                 .Interval(TimeSpan.FromSeconds(1))
-                .Subscribe(fun _ -> 
-                    // similar to newSeries create null entry in 1% of cases
-                    let randomNull = rnd.Next(0, 99)
-                    match randomNull with
-                    | i when i = 0 -> 
-                        dispatch AddNull
-                    | _ -> 
-                        dispatch AddItem
-                    dispatch RemoveItem
+                .Subscribe(fun _ ->
+                    dispatch UpdateSeries
+                    dispatch RemoveOldSeries
                 )
-
         [
-            if model.IsAutoUpdateChecked then
-                [ nameof autoUpdateSub ], autoUpdateSub
+            [ nameof autoUpdateSub ], autoUpdateSub
         ]
 
 open Chart
@@ -158,24 +284,9 @@ type ChartViewModel() as this =
         //Terminate all Elmish subscriptions on dispose (view is registered as Transient).
         |> Program.mkStoreWithTerminate this Terminate 
 
-    do  // Manually disable AutoUpdate (when view is registered as Singleton).
-        this.Subscribe(app.Observable, fun m -> 
-            if m.View <> App.ChartView && this.IsAutoUpdateChecked then
-                local.Dispatch (SetIsAutoUpdateChecked false)
-        )
-
     member this.Series = local.Model.Series
 
-    member this.Actions = this.BindSourceList(local.Model.Actions)
-    member this.AddItem() = local.Dispatch AddItem
-    member this.RemoveItem() = local.Dispatch RemoveItem
-    member this.UpdateItem() = local.Dispatch UpdateItem
-    member this.ReplaceItem() = local.Dispatch ReplaceItem
-    member this.Reset() = local.Dispatch Reset
-    member this.IsAutoUpdateChecked 
-        with get () = this.Bind (local, _.IsAutoUpdateChecked)
-        and set value = local.Dispatch (SetIsAutoUpdateChecked value)
     member this.XAxes = this.Bind (local, fun _ -> XAxes)
-    member this.Ok() = app.Dispatch App.GoHome
+
 
     static member DesignVM = new ChartViewModel()
