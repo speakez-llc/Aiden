b0b89da6440cb5227a6835265d30e0c68e663c88
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-01-01 07:47:58 -0500

    scroll chart initial draft

--- src/AidenDesktop/Views/ZoomView.axaml.fs
+++ src/AidenDesktop/Views/ZoomView.axaml.fs
@@ -4,10 +4,11 @@ open Avalonia
 open Avalonia.Controls
 open Avalonia.Markup.Xaml
 
-type ZoomView () as this = 
+type ZoomView () as this =
     inherit UserControl ()
 
-    do this.InitializeComponent()
-
+    do
+        this.InitializeComponent()
+        
     member private this.InitializeComponent() =
         AvaloniaXamlLoader.Load(this)
