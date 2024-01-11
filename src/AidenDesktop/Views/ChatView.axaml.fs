93ecb534e5899283c5b09eae93503ec2dba2ebb9
Houston Haynes <8174976+houstonhaynes@users.noreply.github.com>
2024-01-10 19:41:38 -0500

    initial KeyDown sensing for Enter submitting TextBox in chat

--- src/AidenDesktop/Views/ChatView.axaml.fs
+++ src/AidenDesktop/Views/ChatView.axaml.fs
@@ -1,13 +1,12 @@
 ﻿namespace AidenDesktop.Views
 
-open System.Diagnostics
+open System
 open Avalonia
+open Avalonia.Input
 open Avalonia.VisualTree
 open Avalonia.Controls
 open Avalonia.Markup.Xaml
-open Avalonia.Threading
 open AidenDesktop.ViewModels
-open System.Collections.Specialized
 
 type ChatView() as this =
     inherit UserControl ()
@@ -18,8 +17,6 @@ type ChatView() as this =
         this.InitializeComponent()
         this.DataContextChanged.Add(fun args ->
             printfn $"DataContextChanged: {args}"
-            
-            //listBox.PropertyChanged.AddHandler(AvaloniaPropertyChangedEventHandler(this.ScrollToBottomSmooth) |> ignore
 
             viewModel <- this.DataContext :?> ChatViewModel |> Some
             match viewModel with
@@ -33,14 +30,9 @@ type ChatView() as this =
                     printfn "Scrolling to bottom"
                     this.ScrollToBottomSmooth())
                 |> ignore
-                (* viewModel.NewMessageEvent
-                |> Observable.subscribe (fun _ ->
-                    printfn "Scrolling to bottom"
-                    this.ScrollToBottomSmooth())
-                |> ignore *)
             | None -> ()
             )
-        
+
     member private this.ScrollToBottomSmooth() =
         let listBox = this.FindControl<ListBox>("ChatWindow")
         if listBox.ItemCount > 0 then
@@ -54,33 +46,18 @@ type ChatView() as this =
                     if targetOffset > scrollViewer.Offset.Y then
                         scrollViewer.Offset <- new Vector(scrollViewer.Offset.X, targetOffset)
                 | None -> ()
-    
 
-    (* member private this.ScrollToBottomSmooth() =
-        printfn "ScrollToBottomSmooth"
-        let listBox = this.FindControl<ListBox>("ChatWindow")
-        if listBox.ItemCount > 0 then
-            let item = listBox.ContainerFromIndex(listBox.ItemCount - 1)
-            let target = (item :?> ListBoxItem).Bounds.Y
-            let scrollViewerOption = listBox.GetVisualDescendants() |> Seq.tryFind (fun v -> v :? ScrollViewer) |> Option.map (fun v -> v :?> ScrollViewer)
-            match scrollViewerOption with
-            | Some scrollViewer ->
-                //scrollViewer.Offset <- new Vector(scrollViewer.Offset.X, target)
-                let sw = Stopwatch.StartNew()
-                let timer = new DispatcherTimer(DispatcherPriority.Render)
-                let start = scrollViewer.Offset.Y
-                let diff = target - start
-                timer.Tick.Add(fun _ ->
-                    let elapsed = sw.Elapsed.TotalMilliseconds
-                    if elapsed < 500.0 then
-                        let offset = start + diff * (elapsed / 500.0)
-                        scrollViewer.Offset <- new Vector(scrollViewer.Offset.X, offset)
-                    else
-                        timer.Stop()
-                        scrollViewer.Offset <- new Vector(scrollViewer.Offset.X, target)
-                )
-                timer.Start()
-            | None -> ()
-*)
+    member this.MessageTextBox_KeyDown(sender: obj, e: KeyEventArgs) =
+        match e.Key with
+        | Key.Enter when e.KeyModifiers = KeyModifiers.None ->
+            let viewModel = this.DataContext :?> ChatViewModel
+            if not (String.IsNullOrEmpty(viewModel.MessageText)) && not (String.IsNullOrWhiteSpace(viewModel.MessageText)) then
+                // Invoke the SendMessage command of the ViewModel
+                viewModel.SendMessage(viewModel.MessageText)
+                e.Handled <- true
+        | _ -> ()
+
+
+
     member private this.InitializeComponent() =
         AvaloniaXamlLoader.Load(this)
